82fdd751d630f6aabb2b922e45d336c20868b771
JimmyFernando <JimmyFernando@Jimmy>
2016-01-16 13:22:38 -0500

    create app for tweet

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'C://wamp//www//symfony-1.4.20//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrineGuardPlugin');
+  }
+}
